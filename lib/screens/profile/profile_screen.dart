4926356d1348329d32e3bf1e3c0345463dd3946a
philz-stizles <theophilusighalo@gmail.com>
2021-07-04 14:35:43 +0100

    Semi fiish

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/enums.dart';
+import 'package:mobile/screens/profile/widgets/profile_menu.dart';
+import 'package:mobile/screens/profile/widgets/profile_pic.dart';
+import 'package:mobile/widgets/widgets.dart';
 
 class ProfileScreen extends StatelessWidget {
-  static String routeName = "/profile-screen";
   const ProfileScreen({Key key}) : super(key: key);
+
+  static String routeName = '/profile-screen';
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: SingleChildScrollView(
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        children: [
+          ProfilePic(),
+          SizedBox(height: 20),
+          ProfileMenu(
+            text: 'My Account',
+            icon: 'assets/icons/User.svg',
+            onPress: () => {},
+          ),
+          ProfileMenu(
+            text: 'Notifications',
+            icon: 'assets/icons/Bell.svg',
+            onPress: () {},
+          ),
+          ProfileMenu(
+            text: 'Settings',
+            icon: 'assets/icons/Settings.svg',
+            onPress: () {},
+          ),
+          ProfileMenu(
+            text: 'Help Center',
+            icon: 'assets/icons/QuestionMark.svg',
+            onPress: () {},
+          ),
+          ProfileMenu(
+            text: 'Log Out',
+            icon: 'assets/icons/Logout.svg',
+            onPress: () {},
+          ),
+        ],
+      ),
+    ),
+      bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.profile),
     );
   }
 }
--- lib/screens/signin_success/signin_success_screen.dart
+++ lib/screens/signin_success/signin_success_screen.dart
@@ -4,43 +4,46 @@ import 'package:mobile/size_config.dart';
 import 'package:mobile/widgets/widgets.dart';
 
 class SignInSuccessScreen extends StatelessWidget {
-  static String routeName = "/signin-success-screen";
+  static String routeName = '/signin-success-screen';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: SizedBox(),
-        title: Text("Login Success"),
-      ),
-      body: Column(
-      children: [
-        SizedBox(height: SizeConfig.screenHeight * 0.04),
-        Image.asset(
-          "assets/images/success.png",
-          height: SizeConfig.screenHeight * 0.4, //40%
-        ),
-        SizedBox(height: SizeConfig.screenHeight * 0.08),
-        Text(
-          "Login Success",
-          style: TextStyle(
-            fontSize: getProportionateScreenWidth(30),
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+      // appBar: AppBar(
+      //   leading: SizedBox(),
+      //   title: Text('Login Success'),
+      // ),
+      body: Center(
+        child: Column(
+        children: [
+          SizedBox(height: SizeConfig.screenHeight * 0.04),
+          Image.asset(
+            'assets/images/success.png',
+            height: SizeConfig.screenHeight * 0.4, //40%
+            fit: BoxFit.cover,
           ),
-        ),
-        Spacer(),
-        SizedBox(
-          width: SizeConfig.screenWidth * 0.6,
-          child: DefaultButton(
-            text: "Back to home",
-            press: () {
-              Navigator.pushNamed(context, HomeScreen.routeName);
-            },
+          SizedBox(height: SizeConfig.screenHeight * 0.08),
+          Text(
+            'Login Success',
+            style: TextStyle(
+              fontSize: getProportionateScreenWidth(30),
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
           ),
-        ),
-        Spacer(),
-      ],
+          Spacer(),
+          SizedBox(
+            width: SizeConfig.screenWidth * 0.6,
+            child: DefaultButton(
+              text: 'Back to home',
+              press: () {
+                Navigator.pushNamed(context, HomeScreen.routeName);
+              },
+            ),
+          ),
+          Spacer(),
+        ],
     ),
+      ),
     );
   }
 }
